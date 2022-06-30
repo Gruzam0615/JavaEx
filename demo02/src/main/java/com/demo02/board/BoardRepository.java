adb4923bfdabc79639662fe632fb2968a71f41cf
Gruzam0615 <gruzam0615@gmail.com>
2022-06-30 18:20:37 +0900

    Update BoardRepository.java

--- demo02/src/main/java/com/demo02/board/BoardRepository.java
+++ demo02/src/main/java/com/demo02/board/BoardRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 	
 	@Query(value="SELECT * FROM board", nativeQuery=true)
